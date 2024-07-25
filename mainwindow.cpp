d6ee9927e5c161cdce21bf4f9e695a0377dbfdff
Massooma <mukhtar.massooma@gmail.com>
2024-07-25 17:25:20 +0200

    Try solve conflict

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,48 +8,6 @@
 #include <QClipboard>
 #include <QFileDialog>
 
-CustomGraphicsView::CustomGraphicsView(QWidget *parent)
-    : QGraphicsView(parent), drawing(false), penColor(Qt::black), penWidth(5)
-{
-}
-
-void CustomGraphicsView::setPenColor(const QColor &color)
-{
-    penColor = color;
-}
-
-void CustomGraphicsView::setPenWidth(int width)
-{
-    penWidth = width;
-}
-
-void CustomGraphicsView::mousePressEvent(QMouseEvent *event)
-{
-    if (event->button() == Qt::LeftButton) {
-        drawing = true;
-        lastPoint = mapToScene(event->pos());
-    }
-}
-
-void CustomGraphicsView::mouseMoveEvent(QMouseEvent *event)
-{
-    if ((event->buttons() & Qt::LeftButton) && drawing) {
-        QPointF currentPoint = mapToScene(event->pos());
-        QPen pen(penColor, penWidth);
-        scene()->addLine(lastPoint.x(), lastPoint.y(), currentPoint.x(), currentPoint.y(), pen);
-        lastPoint = currentPoint;
-    }
-}
-
-void CustomGraphicsView::mouseReleaseEvent(QMouseEvent *event)
-{
-    if (event->button() == Qt::LeftButton && drawing) {
-        drawing = false;
-    }
-}
-
-
-
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
